66d80f52fb2a2eb9a3904d81c92b8393d57dda80
rievxsploit <rifaldiwahyu1697@gmail.com>
2025-05-05 16:32:12 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,12 @@
 // Ganti dengan konfigurasi Firebase milikmu dari Firebase Console
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID"
+ apiKey: "AIzaSyCZqQGh2kXFZ7ARcJi3mD-kXYGhysqet2Y",
+  authDomain: "qrpatrolv2.firebaseapp.com",
+  projectId: "qrpatrolv2",
+  storageBucket: "qrpatrolv2.firebasestorage.app",
+  messagingSenderId: "910596893170",
+  appId: "1:910596893170:web:c44ae1335d0f2b8f6fe36b",
+  measurementId: "G-JKWLJH0CP4"
 };
 
 // Inisialisasi Firebase
