5b824293f18f6cefa9cecfd57dd49d3b05c27f29
rievxsploit <rifaldiwahyu1697@gmail.com>
2025-05-07 14:14:14 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,20 +1,10 @@
 // firebase-config.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
-import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
-import { getFirestore, collection, addDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
-
 const firebaseConfig = {
-  apiKey: "AIzaSyCZqQGh2kXFZ7ARcJi3mD-kXYGhysqet2Y",
-  authDomain: "qrpatrolv2.firebaseapp.com",
-  projectId: "qrpatrolv2",
-  storageBucket: "qrpatrolv2.firebasestorage.app",
-  messagingSenderId: "910596893170",
-  appId: "1:910596893170:web:c44ae1335d0f2b8f6fe36b",
-  measurementId: "G-JKWLJH0CP4"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_BUCKET",
+  messagingSenderId: "YOUR_ID",
+  appId: "YOUR_APP_ID"
 };
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-
-window.firebase = { auth, db, signInWithEmailAndPassword, onAuthStateChanged, signOut, collection, addDoc, serverTimestamp };
+firebase.initializeApp(firebaseConfig);
